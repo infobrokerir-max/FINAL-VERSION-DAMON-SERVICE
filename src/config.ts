56d3fcb39a7ad99905602176b7d8dd48c3d4350e
infobrokerir-max <info.brokerir@gmail.com>
2025-12-27 23:16:14 +0330

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_API = "https://script.google.com/macros/s/AKfycbzH2L14Tno6OZwsy92YJHtr1D0zAT07ug6bHBAZ31krmtntMy5OYrVG0Y5KPaqYWgh/exec";
+export const BASE_API = "https://script.google.com/macros/s/AKfycbzH-2L14Tno6OZwsy92YJHtr1D0zAT07ug6bHBAZ31krmtntMy5OYrVG0Y5KPaqYWgh/exec";
 export const LOGIN_GET_KEY = "DS_BOLT_2025__9f8c7b6a5d4e3c2b1a__XyZ";
